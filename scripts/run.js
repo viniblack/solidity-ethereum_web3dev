caf1652915878f6f23c87ecbaf1e597c95d9398d
Vini Black <viniibl4ck@gmail.com>
2022-07-13 20:10:25 -0300

    criando um smart contract um pouco mais elaborado

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,29 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+  let whowaved = [];
+
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  whowaved.push(waveTxn.from);
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+  whowaved.push(waveTxn.from);
+
+  waveCount = await waveContract.getTotalWaves();
+  console.log(`Essas foram as pessoas que acenaram:\n${whowaved}`);
 };
 
 const runMain = async () => {
