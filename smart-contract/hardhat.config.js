217f87a666abddfe3cd75f944dc9c096dcacd0ce
Vini Black <viniibl4ck@gmail.com>
2022-07-13 23:51:47 -0300

    implementando o front-end

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,21 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/mb2egAs4nUD4anEqXfE6mmbvRjuBbyEe",
+      accounts: ["ab05dc99c1eaac691bdb8130f62d8e8d9e88817ed79f90025437de2b043e1f7f"],
+    },
+  },
 };
